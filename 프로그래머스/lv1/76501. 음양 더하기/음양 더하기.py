b67ea6cc3008ca87d462e02e4e172d44475b7a2c
Jimmy <108419421+jeongjinmyung@users.noreply.github.com>
2022-10-06 14:50:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.30 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            answer += int(absolutes[i])
+        elif signs[i] == False:
+            answer -= int(absolutes[i])
+    return answer
