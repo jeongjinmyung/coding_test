9831d7f6de5d3ba1aa07e51133f0be9b5587228f
Jimmy <108419421+jeongjinmyung@users.noreply.github.com>
2022-10-28 19:17:16 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,10 @@
+def solution(ingredient):
+    stack = []
+    cnt = 0
+    for i in ingredient:
+        stack.append(i)
+        if stack[-4:] == [1,2,3,1]:
+            del stack[-4:]
+            cnt +=1
+    return cnt
+
