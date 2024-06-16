582f836b2cfc48f38947ad2792e0e7f2f0a95d65
Jimmy <108419421+jeongjinmyung@users.noreply.github.com>
2024-06-16 23:33:35 +0900

    [level 3] Title: 풍선 터트리기, Time: 695.59 ms, Memory: 63.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -0,0 +1,19 @@
+def solution(a):
+    answer = 0
+    n = len(a)
+    if n <= 2:
+        return n
+    left_min = [0] * len(a)
+    right_min = [0] * len(a)
+
+    left_min[0] = a[0]
+    for i in range(1, n):
+        left_min[i] = min(left_min[i-1], a[i])
+    right_min[n-1] = a[n-1]
+    for i in range(n-2, -1, -1):
+        right_min[i] = min(right_min[i+1], a[i])
+    for i in range(n):
+        if a[i] <= left_min[i] or a[i] <= right_min[i]:
+            answer += 1
+    
+    return answer
