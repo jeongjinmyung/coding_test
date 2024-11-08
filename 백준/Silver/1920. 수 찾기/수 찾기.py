e8065150fb2bdb4e047f13623efcc4d860cd798c
Jimmy <108419421+jeongjinmyung@users.noreply.github.com>
2024-11-08 22:42:18 +0900

    [Silver IV] Title: 수 찾기, Time: 404 ms, Memory: 47416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,24 +1,29 @@
-n = int(input())
-smpl = list(map(int,input().split()))
+import sys
+input = sys.stdin.readline
 
-m = int(input())
-comp = list(map(int,input().split()))
+N = int(input())
+a_list = list(map(int, input().split()))
+M = int(input())
+b_list = list(map(int, input().split()))
 
-smpl.sort()
+a_list.sort()
 
-def bisearch(num, target):
-    start = 0
-    end = n -1
+def is_in(n: int, target: list) -> bool:
+    left, right = 0, len(target)-1
     
-    while start <= end:
-        mid = (start + end) // 2
-        if num[mid] == target:
-            return 1
-        elif num[mid] > target:
-            end = mid -1
-        elif num[mid] < target:
-            start = mid + 1
-    return 0
+    while left <= right:
+        mid = (left + right) // 2
+        if n == target[mid]:
+            return True
+        elif n > target[mid]:
+            left = mid + 1
+        elif n < target[mid]:
+            right = mid -1
+    
+    return False
 
-for t in comp:
-    print(bisearch(smpl, t))
+for i in range(M):
+    if is_in(b_list[i], a_list):
+        print(1)
+    else:
+        print(0)
